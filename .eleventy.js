2309305792daa037a907894e0b5c7712158cc3e1
arun99 <gabbarsan99@gmail.com>
2023-03-29 04:51:10 +1100

    removed the redundant css and their references

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,5 @@
 module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("./src/style.css");
-    eleventyConfig.addPassthroughCopy("./src/assets/templates/css/bootstrap.min.css");
-    eleventyConfig.addPassthroughCopy("./src/assets/templates/css/datepicker.css");
-    eleventyConfig.addPassthroughCopy("./src/assets/templates/css/extralayers.css");
-    eleventyConfig.addPassthroughCopy("./src/assets/templates/css/navstylechange.css");
-    eleventyConfig.addPassthroughCopy("./src/assets/templates/css/owl.carousel.css");
-    eleventyConfig.addPassthroughCopy("./src/assets/templates/css/owl.theme.css");
-    eleventyConfig.addPassthroughCopy("./src/assets/templates/css/style.css");
-    eleventyConfig.addPassthroughCopy("./src/assets/templates/css/superfish.css");
-    eleventyConfig.addPassthroughCopy("./src/assets/templates/css/weather.css");
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
 
