a94db4a7ab152421ae0dc17a1700d772d508e2f1
arun99 <gabbarsan99@gmail.com>
2023-04-10 06:30:33 +1000

    added common data

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,13 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
+    eleventyConfig.addPassthroughCopy("./src/common/price-panel-list.json");
 
     return {
         dir: {
             input: "src",
+            includes: "_includes",
+            data: "common-data",
             output: "public"
         }
     };
